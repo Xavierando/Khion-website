6c7d54475dbe56f0cc787c83c621919e86e303a0
Xavier Prot <prot.xavier@gmail.com>
2025-08-02 22:40:08 +0200

    fix product resources

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'product',
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'description' => $this->description,
+            'base_price' => $this->base_price,
+            'imageUrl' => '/images/'.$this->imageUrl,
+            'short' => str($this->description)->words(20, '...'),
+            'link' => '/products/'.$this->id,
+            'configuration' => $this->configuration
+        ];
+    }
+}
