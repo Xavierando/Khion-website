280691eba2e6ebf2ea6f1a386d5da655deaaea97
Xavier Prot <prot.xavier@gmail.com>
2025-08-02 22:40:21 +0200

    fix user resources

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'user',
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'isTeam' => $this->isTeam,
+            'bio' => $this->bio,
+            'role' => $this->role,
+            'url' => $this->imageUrl,
+        ];
+    }
+}
