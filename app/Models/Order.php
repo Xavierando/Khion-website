79227513255c148eafc74bce8ef4cda81cb9167d
Xavier Prot <prot.xavier@gmail.com>
2025-08-08 17:55:16 +0200

    add user-order relations

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Enums\OrderStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
@@ -21,5 +22,10 @@ class Order extends Model
     public function order_items(): HasMany
     {
         return $this->hasMany(Order_item::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
