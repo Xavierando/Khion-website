97af5fa04690ecb61566c2f5090587808300c861
Xavier Prot <prot.xavier@gmail.com>
2025-08-25 21:46:14 +0200

    add validation rule for updating product

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -11,7 +11,7 @@ class UpdateProductRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,15 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'code' => 'string',
+            'name' => 'string',
+            'description' => 'string',
+            'base_price' => 'integer',
+            'quantity' => 'integer',
+            'images' => 'Array',
+            'images.*' => 'image',
+            'deleteImage' => 'string|nullable',
+            'update' => 'required|string',
         ];
     }
 }
