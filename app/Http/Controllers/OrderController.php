b804de74df1a9007497f1a9cbd07e569b7441368
Xavier Prot <prot.xavier@gmail.com>
2025-08-27 17:34:34 +0200

    add sorting to ordred list

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -6,6 +6,7 @@ use App\Enums\OrderStatus;
 use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
 
@@ -13,7 +14,8 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orders = $request->user()->orders()->with('order_items')->paginate(15);
+        $orderByField = DB::raw('FIELD(status, '.substr(array_reduce(OrderStatus::cases(), fn ($carry, $item) => $carry .= ",'".$item->value."'", ''), 1).')');
+        $orders = $request->user()->orders()->with('order_items')->orderBy($orderByField)->orderBy('created_at')->paginate(15);
 
         return Inertia::render('dashboard/orders/index', [
             'orders' => OrderResource::collection($orders),
