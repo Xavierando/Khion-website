b3f16304891bd935e62475497ee1ed895a74475d
Xavier Prot <prot.xavier@gmail.com>
2025-08-29 00:09:00 +0200

    add delete order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,9 +14,12 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        $orderByField = DB::raw('FIELD(status, '.substr(array_reduce(OrderStatus::cases(), fn ($carry, $item) => $carry .= ",'".$item->value."'", ''), 1).')');
-        $orders = $request->user()->orders()->with('order_items')->orderBy($orderByField)->orderBy('created_at')->paginate(15);
-
+        $orderByField = DB::raw('FIELD(status, ' . substr(array_reduce(OrderStatus::cases(), fn($carry, $item) => $carry .= ",'" . $item->value . "'", ''), 1) . ')');
+        if ($request->user()->isAdmin) {
+            $orders = Order::with('order_items')->orderBy($orderByField)->orderBy('created_at')->paginate(15);
+        } else {
+            $orders = $request->user()->orders()->with('order_items')->orderBy($orderByField)->orderBy('created_at')->paginate(15);
+        }
         return Inertia::render('dashboard/orders/index', [
             'orders' => OrderResource::collection($orders),
         ]);
@@ -26,7 +29,6 @@ class OrderController extends Controller
     {
 
         $user = $request->user();
-        Log::debug($user->isAdmin);
         if (! $user->isAdmin && $user->id != $order->user_id) {
             return redirect()->back();
         }
@@ -34,6 +36,20 @@ class OrderController extends Controller
         if ($request->has('status') && $user->isAdmin) {
             $status = $request->enum('status', OrderStatus::class);
             $order->statusUpdate($status);
+        }
+
+        return redirect()->route('dashboard.order');
+    }
+
+    public function delete(Request $request, Order $order)
+    {
+        $user = $request->user();
+        if (! $user->isAdmin && $user->id != $order->user_id) {
+            return redirect()->back();
+        }
+
+        if ($order->status == OrderStatus::pending) {
+            $order->delete();
         }
 
         return redirect()->route('dashboard.order');
