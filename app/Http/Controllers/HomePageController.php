7241e8c9c1c00d33e5b219f7cce190b3a494d5a6
Xavier Prot <prot.xavier@gmail.com>
2025-08-30 12:41:44 +0200

    filter only available product displayed in homepage

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -17,12 +17,17 @@ class HomePageController extends Controller
     {
 
         $teams = UserResource::collection(User::where('isTeam', '=', '1')->get());
-        $products = ProductResource::collection(Product::orderBy('created_at', 'desc')->take(4)->get());
+        $products = ProductResource::collection(Product::orderBy('created_at', 'desc')->get());
+        $fproducts = $products->filter(function ($v) {
+            return $v->availableQuantity() > 0;
+        })->shift(4);
 
-        return Inertia::render('Welcome', [
-            'teams' => $teams,
-            'products' => $products,
-        ]
+        return Inertia::render(
+            'Welcome',
+            [
+                'teams' => $teams,
+                'products' => $fproducts,
+            ]
         );
     }
 }
