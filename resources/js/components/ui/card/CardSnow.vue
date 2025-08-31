0315905e548fc787490c863907c05b5ee4336298
Xavier Prot <prot.xavier@gmail.com>
2025-08-31 11:02:47 +0200

    extend clickable area to whole card

--- resources/js/components/ui/card/CardSnow.vue
+++ resources/js/components/ui/card/CardSnow.vue
@@ -11,30 +11,34 @@ const props = defineProps<{
 
 <template>
   <Transition appear>
-    <div
-      :class="cn('max-w-md w-full mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg grid grid-col-1 grid-template-columns: max-content 1fr', props.class)">
-      <div class="relative">
-        <img class="w-full" :src="product.default_images?.src" alt="Product Image">
-        <div v-if="product.quantity === 0" class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">
-          SOLD OUT
+    <a :href="product.link">
+      <div
+        :class="cn('max-w-md w-full mx-auto rounded-md overflow-hidden shadow-md hover:shadow-lg grid grid-col-1 grid-template-columns: max-content 1fr', props.class)">
+        <div class="relative">
+          <img class="w-full" :src="product.default_images?.src" alt="Product Image">
+          <div v-if="product.quantity === 0"
+            class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">
+            SOLD OUT
+          </div>
+        </div>
+        <div class="p-4 flex flex-col justify-between">
+          <div>
+            <h3 class="text-lg font-medium mb-2">{{ product.name }}</h3>
+          </div>
+          <div class="grow mb-4 flex flex-row flex-wrap justify-between gap-2 items-start">
+            <button class="text-gray-600 text-sm bg-blue-200 rounded-sm p-1 px-2" v-for="tag in product.tags"
+              :key="tag.id">{{ tag.tag }}</button>
+          </div>
+          <div class="flex items-center justify-between">
+            <span class="font-bold text-lg">{{ product.base_price }} &euro;</span>
+            <a :href="product.link">
+              <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded cursor-pointer">
+                Dettagli
+              </button>
+            </a>
+          </div>
         </div>
       </div>
-      <div class="p-4 flex flex-col justify-between">
-        <div>
-          <h3 class="text-lg font-medium mb-2">{{ product.name }}</h3>
-        </div>
-        <div class="grow mb-4 flex flex-row flex-wrap justify-between gap-2 items-start">
-          <button class="text-gray-600 text-sm bg-blue-200 rounded-sm p-1 px-2" v-for="tag in product.tags" :key="tag.id">{{ tag.tag }}</button>
-        </div>
-        <div class="flex items-center justify-between">
-          <span class="font-bold text-lg">{{ product.base_price }} &euro;</span>
-          <a :href="product.link">
-            <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded cursor-pointer">
-              Dettagli
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
+    </a>
   </Transition>
 </template>
