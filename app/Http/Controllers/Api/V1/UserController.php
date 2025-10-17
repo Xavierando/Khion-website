9ddd3fc274790d5a02112a4d302e2957580e7a79
Xavier Prot <prot.xavier@gmail.com>
2025-10-17 13:18:42 +0200

    add user resource endpoint

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Resources\UserResource;
+use App\Traits\ApiResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Inertia\Inertia;
+
+class UserController extends ApiController
+{
+    use ApiResponses;
+    //
+    public function update(Request $request)
+    {
+
+        $user = $request->user();
+        $responseStatus = 'failed';
+
+        if ($request->has(['name', 'role', 'bio'])) {
+            $validated = $request->validate([
+                'name' => 'required|string',
+                'role' => 'string|nullable',
+                'bio' => 'string|nullable',
+            ]);
+            $user->update($validated);
+            $responseStatus = 'success';
+        }
+
+        if ($request->has('oldpsw', 'newpsw')) {
+            $validated = $request->validate([
+                'oldpsw' => 'required|string',
+                'newpsw' => 'required|string|min:8',
+            ]);
+            if (Hash::check($validated['oldpsw'], $user->password)) {
+                $user->update(['password' => Hash::make($validated['newpsw'])]);
+                $responseStatus = 'success';
+            }
+        }
+
+        if ($request->hasFile('pic')) {
+            $request->validate([
+                'pic' => 'required|image|mimes:jpg,jpeg,png|max:512',
+            ]);
+
+            $path = Storage::disk('images')->put('pic/', $request->file('pic'));
+            Storage::disk('images')->delete($user->imageUrl);
+            $user->update(['imageUrl' => $path]);
+            $responseStatus = 'success';
+        }
+
+        return $this->ok($responseStatus, ['user' => new UserResource($user)]);
+    }
+}
