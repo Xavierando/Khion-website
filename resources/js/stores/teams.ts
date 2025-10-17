c0ca30d0f3d0eca9c4ab0cc6d90e359cbedd2b52
Xavier Prot <prot.xavier@gmail.com>
2025-10-17 13:30:51 +0200

    add teams storage

---
+++ resources/js/stores/teams.ts
@@ -0,0 +1,37 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+export const useTeamsStore = defineStore('teams', {
+  state: () => {
+    return {
+      teamsMember: null,
+    } as teams
+  },
+  actions: {
+    async fetchAllTeamsMembers() {
+      try {
+        const response = await axios.get('/api/teams')
+        if (response.request.status === 200) {
+          if (response.data.message === 'success') {
+            this.teamsMember = response.data.data.teamsMembers;
+          }
+        }
+      } catch (error) {
+        // let the form component display the error
+        return error
+      }
+    },
+  }
+})
+
+interface teams {
+  teamsMember: teamsMember[] | null,
+}
+
+export interface teamsMember {
+  type : string
+  name: string
+  url: string
+  bio: string
+  role: string
+}
