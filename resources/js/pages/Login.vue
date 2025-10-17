136ed945b8347f3af54e790f6b3ff1d8839228df
Xavier Prot <prot.xavier@gmail.com>
2025-10-17 13:32:11 +0200

    add login page

---
+++ resources/js/pages/Login.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import PageSection from '@/components/ui/section/PageSection.vue';
+import { useAuthStore } from '@/stores/auth';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
+const auth = useAuthStore();
+const router = useRouter();
+const email = ref('');
+const password = ref('');
+
+
+
+const redirect = () => {
+    if (auth.loggedIn) {
+        router.push({ path: '/' })
+    }
+}
+
+auth.refresh().then(redirect)
+</script>
+
+<template>
+    <PageSection class="h-screen align-center">
+        <v-card max-width="500" class="mx-auto pa-6" color="primary">
+            <v-sheet class="d-flex justify-center bg-transparent pb-6">
+            <v-icon icon="mdi-snowflake" size="120" class="mx-auto"></v-icon>
+            </v-sheet>
+            <v-form @submit.prevent="auth.login(email, password).then(redirect)">
+                <v-text-field v-model="email" label="Email address" type="email"
+                    :rules="auth.errors?.email"></v-text-field>
+                <v-text-field v-model="password" label="Password" type="password"
+                    :rules="auth.errors?.password"></v-text-field>
+
+                <v-btn class="mt-2 pa-2 ml-auto mr-auto" type="submit" block variant="elevated" size="large"
+                    :loading="auth.loggingIn">Submit</v-btn>
+            </v-form>
+        </v-card>
+    </PageSection>
+
+</template>
