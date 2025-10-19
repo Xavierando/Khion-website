913bf6059fb87ba5ef34a11f53411218ca5292ba
Xavier Prot <prot.xavier@gmail.com>
2025-10-19 22:33:49 +0200

    clenup unused layout class instruction

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -5,7 +5,7 @@ import NavBar from './components/ui/nav/NavBar.vue';
 </script>
 
 <template>
-    <v-sheet class="h-screen tw:bg-[url(@images/homepage.jpg)]  tw:bg-cover bg">
+    <v-sheet>
         <NavBar />
         <router-view v-slot="{ Component, route }">
             <div :key="route.name">
