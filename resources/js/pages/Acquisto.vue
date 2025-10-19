d659ff19c711ec6b241b3266db692cad4150d2f8
Xavier Prot <prot.xavier@gmail.com>
2025-10-19 22:36:28 +0200

    add return page for stripe checkout

---
+++ resources/js/pages/Acquisto.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import PageSection from '@/components/ui/section/PageSection.vue';
+import { useAuthStore } from '@/stores/auth';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
+
+const props = defineProps<{
+    session_id: string,
+}>()
+
+const auth = useAuthStore();
+const router = useRouter();
+const email = ref('');
+const password = ref('');
+
+auth.refresh();
+
+</script>
+
+<template>
+    <PageSection class="mt-16">
+
+    </PageSection>
+
+</template>
